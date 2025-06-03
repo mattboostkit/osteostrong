310d96246cfd85afd54bd536ee8acfca6146f65c
mattboostkit <matt@boostkit.io>
2025-06-03 22:04:03 +0100

    Logos

--- client/src/components/sections/AsSeenOn.tsx
+++ client/src/components/sections/AsSeenOn.tsx
@@ -1,8 +1,8 @@
 const logos = [
-  { name: "Forbes", src: "/forbes.svg" },
-  { name: "Tony Robbins", src: "/tony-robbins.svg" },
-  { name: "TWBM", src: "/twbm.png" },
-  { name: "Wealden Times", src: "/wealden-times.png" },
+  { name: "Forbes", src: "https://ik.imagekit.io/boostkit/OsteoStrong/forbes.svg?updatedAt=1748984505896" },
+  { name: "Tony Robbins", src: "https://ik.imagekit.io/boostkit/OsteoStrong/tony-robbins.svg?updatedAt=1748984506088" },
+  { name: "TWBM", src: "https://ik.imagekit.io/boostkit/OsteoStrong/twbm.png?updatedAt=1748984505747" },
+  { name: "Wealden Times", src: "https://ik.imagekit.io/boostkit/OsteoStrong/wealden-times.png?updatedAt=1748984505800" },
 ];
 
 const AsSeenOn = () => {
